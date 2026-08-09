cba9ce148ee675a84f659024d0362f47193e5230
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-08-09 00:51:34 +0000

    Update OCP

--- NCU/Sharing/ISharingManager.php
+++ NCU/Sharing/ISharingManager.php
@@ -13,9 +13,7 @@ use NCU\Sharing\Exception\ShareInvalidException;
 use NCU\Sharing\Exception\ShareNotFoundException;
 use NCU\Sharing\Exception\ShareOperationForbiddenException;
 use NCU\Sharing\Permission\ISharePermissionPreset;
-use NCU\Sharing\Permission\ISharePermissionType;
 use NCU\Sharing\Permission\SharePermission;
-use NCU\Sharing\Property\ISharePropertyType;
 use NCU\Sharing\Property\ShareProperty;
 use NCU\Sharing\Recipient\IShareRecipientType;
 use NCU\Sharing\Recipient\ShareRecipient;
@@ -50,12 +48,11 @@ interface ISharingManager {
 	public function generateSecret(): string;
 
 	/**
-	 * Generate a new timestamp in milliseconds since the UNIX epoch.
+	 * Get the current time
 	 *
-	 * @return non-negative-int
 	 * @experimental 35.0.0
 	 */
-	public function generateTimestamp(): int;
+	public function getTime(): \DateTimeImmutable;
 
 	/**
 	 * Create a new share.
@@ -151,13 +148,6 @@ interface ISharingManager {
 	public function updateShareRecipientSecret(ShareAccessContext $accessContext, string $id, ShareRecipient $recipient, string $secret): void;
 
 	/**
-	 * @param class-string<ISharePropertyType> $propertyTypeClass
-	 * @throws ShareNotFoundException
-	 * @experimental 35.0.0
-	 */
-	public function createSharePropertyDefaultValue(Share $share, string $propertyTypeClass): Share;
-
-	/**
 	 * Update a property of a share.
 	 *
 	 * @throws ShareInvalidException
@@ -166,13 +156,6 @@ interface ISharingManager {
 	 * @experimental 35.0.0
 	 */
 	public function updateShareProperty(ShareAccessContext $accessContext, string $id, ShareProperty $property): void;
-
-	/**
-	 * @param class-string<ISharePermissionType> $permissionTypeClass
-	 * @throws ShareNotFoundException
-	 * @experimental 35.0.0
-	 */
-	public function createSharePermissionDefaultValue(Share $share, string $permissionTypeClass): Share;
 
 	/**
 	 * Update a permission of a share.
